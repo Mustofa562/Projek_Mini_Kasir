776ddcd8c89ed007bf140200136f6887da7ce3b4
Mutofa <mustofa9731@gmail.com>
2025-12-08 20:39:32 +0700

    tambah fitur lagi

--- Menu.py
+++ Menu.py
@@ -1,45 +1,68 @@
 from Database import barang,cart,lihat_data,laporan_penjualan
 from New_fitur import tambah_produk,hapus_produk,Update_stok,Laporan_bulanan,Grafik_penjualan
 
-while True:
-    print("""
-    Kasir mini
-    1. Lihat menu
-    2. Pilih menu
-    3. Keranjang Menu
-    4. Tambah Produk
-    5. Hapus Produk
-    6. Update Stok/Harga
-    7. Laporan penjualan harian
-    8. Laporan penjualan Bulanan
-    9. Grafik penjualan Harian
-    """)
+def main_menu():
+    while True:
+        print("""
+        Kasir mini
+        1. Lihat menu
+        2. Pilih menu
+        3. Keranjang Menu
+        4. Tambah Produk
+        5. Hapus Produk
+        6. Update Stok/Harga
+        7. Laporan penjualan harian
+        8. Laporan penjualan Bulanan
+        9. Grafik penjualan Harian/Bulanan
+        """)
 
-    user = input('Pilih salah satu menu : ')
+        user = input('Pilih salah satu menu : ')
 
-    if user == '1':
-        lihat_data()
+        if user == '1':
+            lihat_data()
 
-    elif user == '2':
-        barang()
+        elif user == '2':
+            barang()
 
-    elif user == '3':
-        cart()
+        elif user == '3':
+            cart()
 
-    elif user == '4':
-        tambah_produk()
+        elif user == '4':
+            tambah_produk()
 
-    elif user == '5':
-        hapus_produk()
+        elif user == '5':
+            hapus_produk()
 
-    elif user == '6':
-        Update_stok()
+        elif user == '6':
+            Update_stok()
 
-    elif user == '7':
-        laporan_penjualan()
+        elif user == '7':
+            laporan_penjualan()
 
-    elif user == '8':
-        Laporan_bulanan()
+        elif user == '8':
+            Laporan_bulanan()
 
-    elif user == '9':
-        Grafik_penjualan()
+        elif user == '9':
+            Grafik_penjualan()
+
+#Validasi input
+def input_angka (pesan):
+    while True:
+        nilai = input(pesan)
+        if nilai.isdigit():
+            return int(nilai)
+        print("Input harus angka!")
+
+def input_yn (pesan):
+    while True:
+        jawab = input(pesan).lower()
+        if jawab in ['y','n']:
+            return jawab
+        print('Pilihan harus (y/n)!')
+
+def input_menu(max):
+    while True:
+        nilai = input('Pilih menu: ')
+        if nilai.isdigit() and 1 <= int(nilai) <= max:
+            return int (nilai)
+        print(f"Pilihan menu harus angka 1 - {max}")
---
+++ main.py
@@ -0,0 +1,4 @@
+from Menu import main_menu
+
+if __name__ == "__main__":
+    main_menu()
